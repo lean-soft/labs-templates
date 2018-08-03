8c41397b7bd07bc10c28635eb742fa9b311b9651
周文洋(Jacky Zhou) <jackyzhou@lean-soft.cn>
2018-08-03 15:28:34 +0800

    Merged PR 217: Merge dev to master

--- LabsProject-yoteam-create-project.ps1
+++ LabsProject-yoteam-create-project.ps1
@@ -2,12 +2,16 @@ yo team:project --skip-cache false "$env:applicationName$env:BUILD_BUILDNUMBER" 
 
 yo team:azure --skip-cache false "$env:applicationName$env:BUILD_BUILDNUMBER" "$env:tfs" "$env:azureSub" "$env:azureSubId" "$env:tenantId" "$env:servicePrincipalId" "$env:servicePrincipalKey" "$env:pat"
 
-yo team:build --skip-cache false asp "$env:applicationName$env:BUILD_BUILDNUMBER" "$env:tfs" "$env:queue" "$env:target" "$env:dockerHost" "$env:dockerRegistry" "$env:dockerRegistryId" "$env:pat" "$env:applicationName"
+yo team:build --skip-cache false "$env:type" "$env:applicationName$env:BUILD_BUILDNUMBER" "$env:tfs" "$env:queue" "$env:target" "$env:dockerHost" "$env:dockerRegistry" "$env:dockerRegistryId" "$env:pat" "$env:applicationName"
 
-yo team:pipeline --skip-cache false asp "$env:applicationName$env:BUILD_BUILDNUMBER" "$env:tfs" "$env:queue" "$env:target" "$env:azureSub" "$env:azureSubId" "$env:tenantId" "$env:servicePrincipalId" "$env:dockerHost" "$env:dockerCertPath" "$env:dockerRegistry" "$env:dockerRegistryId" "$env:dockerPorts" "$env:dockerRegistryPassword" "$env:servicePrincipalKey" "$env:pat" "$env:customFolder"
+yo team:pipeline --skip-cache false "$env:type" "$env:applicationName$env:BUILD_BUILDNUMBER" "$env:tfs" "$env:queue" "$env:target" "$env:azureSub" "$env:azureSubId" "$env:tenantId" "$env:servicePrincipalId" "$env:dockerHost" "$env:dockerCertPath" "$env:dockerRegistry" "$env:dockerRegistryId" "$env:dockerPorts" "$env:dockerRegistryPassword" "$env:servicePrincipalKey" "$env:pat" "$env:customFolder"
 
 yo team:git --skip-cache false "$env:applicationName$env:BUILD_BUILDNUMBER" "$env:tfsHttp" clone "$env:pat"
-yo team:asp --skip-cache false "$env:applicationName$env:BUILD_BUILDNUMBER"  false "$env:dockerPorts"
+yo team:$env:type --skip-cache false "$env:applicationName$env:BUILD_BUILDNUMBER"  false "$env:dockerPorts"
 yo team:git --skip-cache false "$env:applicationName$env:BUILD_BUILDNUMBER" "$env:tfsHttp"  commit "$env:pat"
 cd $env:applicationName$env:BUILD_BUILDNUMBER
+
+
+# when type is aspFull,config postBuffer fix error: fatal: The remote end hung up unexpectedly
+git config --global http.postBuffer 524288000
 git push
